e670f70848a3ba4f4a58e77e8ecea2e2750af4f9
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-11-18 22:01:12 +0300

    news api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 using BestStoreMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json.Serialization;
 
 namespace BestStoreMVC.Controllers
 {
-    public class HomeController : Controller
+    public partial class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -21,6 +22,25 @@ namespace BestStoreMVC.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> News()
+        {
+
+            Root root = new Root();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage message = await client.GetAsync("https://newsapi.org/v2/everything?q=tesla&from=2024-10-15&sortBy=publishedAt&apiKey=380d97d4ee3344da84c022ffb2bc66d9");
+
+            if (message.IsSuccessStatusCode)
+            {
+                var jstring = await message.Content.ReadAsStringAsync();
+               root = JsonConverter.DeserializeObject <Root>(jstring);
+                return View(root);
+            }
+            else
+            {
+                return View(root);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
